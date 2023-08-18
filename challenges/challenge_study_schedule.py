a795505131361a84cef4b39c2630a52029fd1fc4
Allyson Bogo <ab.bogo@gmail.com>
2023-08-18 15:22:25 -0300

    implements study_schedule function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for permanence in permanence_period:
+        entered = permanence[0]
+        left = permanence[1]
+
+        if type(entered) != int or type(left) != int:
+            return None
+
+        if entered <= target_time and left >= target_time:
+            count += 1
+
+    return count
